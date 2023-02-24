93183205fef113746abf79e4553fb2fbb6aa6bfc
Mrowr Purr <mrowr.purr@gmail.com>
2023-02-23 17:09:05 -0800

    Library can eject itself

--- Fallout1/DllInjector/LibraryToInject.cpp
+++ Fallout1/DllInjector/LibraryToInject.cpp
@@ -1,14 +1,24 @@
 #include <windows.h>
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+#include <chrono>
+#include <thread>
+
+DWORD __stdcall mainThread(PVOID base) {
+    auto module = static_cast<HMODULE>(base);
+    MessageBoxA(0, "CHANGE IT AGAIN", "DLL Message", MB_OK);
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    FreeLibraryAndExitThread(module, 0);
+}
+
+int __stdcall DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
-            MessageBoxA(0, "Let's gooooooooooooooooooooooo!", "DLL Message", MB_OK);
+            CreateThread(nullptr, 0, mainThread, hModule, 0, nullptr);
             break;
         case DLL_THREAD_ATTACH:
         case DLL_THREAD_DETACH:
         case DLL_PROCESS_DETACH:
             break;
     }
-    return TRUE;
+    return 1;
 }
