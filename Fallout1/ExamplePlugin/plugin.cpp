d6bfa225ad4f30c4c74a5e8d0328070eb7cfe6dd
Mrowr Purr <mrowr.purr@gmail.com>
2023-02-25 21:59:42 -0800

    Setup Detour for EntityList tracking

--- Fallout1/ExamplePlugin/plugin.cpp
+++ Fallout1/ExamplePlugin/plugin.cpp
@@ -1,27 +1,61 @@
 #include <DllInjector/DllInjector.h>
 #include <Fallout1.h>
+#include <psapi.h>  // For MODULEINFO
 #include <unknwn.h>
 
 #include "Print.h"
 
 using namespace Fallout1;
 
-void PrintOutFalloutStuff() {
-    auto& player = Fallout1::GetPlayer();
-
-    Print("NAME: {}", player.GetName());
-    Print("AGE: {}", player.GetAge());
-    auto count = player.GetInventoryCount();
-    Print("INVENTORY COUNT: {}", count);
-
-    auto items = player.GetInventoryItems();
-    for (auto& item : items) {
-        Print("ITEM QUANTITY: {}", item.quantity);
-        Print("ITEM PID: {}", item.item->prototypeID);
-    }
+MODULEINFO GetModuleInfo(const wchar_t* szModule) {
+    MODULEINFO modinfo = {0};
+    HMODULE    hModule = GetModuleHandle(szModule);
+    if (hModule == 0) return modinfo;
+    GetModuleInformation(GetCurrentProcess(), hModule, &modinfo, sizeof(MODULEINFO));
+    return modinfo;
 }
 
-bool Detour32(BYTE* src, BYTE* dst, const uintptr_t len) {
+DWORD FindPattern(const wchar_t* module, unsigned int startOffset, const char* pattern, const char* mask) {
+    // Get all module related information
+    MODULEINFO mInfo = GetModuleInfo(module);  // GetModuleInfoA for char* ?
+
+    // Assign our base and module size
+    // Having the values right is ESSENTIAL, this makes sure
+    // that we don't scan unwanted memory and leading our game to crash
+    DWORD base = (DWORD)mInfo.lpBaseOfDll + startOffset;
+    DWORD size = (DWORD)mInfo.SizeOfImage;
+
+    // Get length for our mask, this will allow us to loop through our array
+    DWORD patternLength = (DWORD)strlen(mask);
+
+    for (DWORD i = 0; i < size - patternLength; i++) {
+        bool found = true;
+        for (DWORD j = 0; j < patternLength; j++) {
+            // if we have a ? in our mask then we have true by default,
+            // or if the bytes match then we keep searching until finding it or not
+            if (mask[j] == '?') continue;
+            try {
+                auto value = *(char*)(base + i + j);
+                if (value != pattern[j]) {
+                    found = false;
+                    break;
+                }
+            } catch (...) {
+                Print("At least try/catch got it??? i:{} j:{}", i, j);
+            }
+        }
+
+        // found = true, our entire pattern was found
+        // return the memory addy so we can write to it
+        if (found) {
+            return base + i;
+        }
+    }
+
+    return NULL;
+}
+
+bool Detour32(BYTE* src, BYTE* dst, const uintptr_t len = 5) {
     if (len < 5) return false;
     DWORD curProtection;
     VirtualProtect(src, len, PAGE_EXECUTE_READWRITE, &curProtection);
@@ -58,6 +92,34 @@ void __declspec(naked) HookFunction() {
     }
 }
 
+DWORD collectEntitiesJumpBackAddy;
+
+void __declspec(naked) CollectEntitiesHookFunction() {
+    Print("Hello from the hook! Can we get entities????");
+    __asm {
+        add esp,8
+        test edx,edx
+        jmp [jumpBackAddy]
+    }
+}
+
+void FindEntityList() {
+    auto module              = L"falloutwHR.exe";
+    auto signature           = "\x8B\x72\x04\xEB\x11\xBB\x01\x00\x00\x00\xBA\x00\x00\x00\x00\x89\xC8";
+    auto mask                = "xxxxxxxxxxx????xx";
+    auto startOffset         = 0x10000;
+    auto signatureAddress    = FindPattern(module, startOffset, signature, mask);
+    auto detourTargetAddress = signatureAddress - 0x7;
+    jumpBackAddy             = signatureAddress - 0x2;
+
+    Print("Signature address: {:x}", signatureAddress);
+    Print("Detour target address: {:x}", detourTargetAddress);
+    Print("Jump back address: {:x}", jumpBackAddy);
+
+    Detour32((BYTE*)detourTargetAddress, (BYTE*)CollectEntitiesHookFunction);
+    Print("Detour added!");
+}
+
 void DoHookingBadassery() {
     Print("Hooking badassery");
     auto hookLength      = 5;
@@ -67,7 +129,32 @@ void DoHookingBadassery() {
     Print("Detour added!");
 }
 
+void PrintOutFalloutStuff() {
+    auto& player = Fallout1::GetPlayer();
+
+    Print("NAME: {}", player.GetName());
+    Print("AGE: {}", player.GetAge());
+    auto count = player.GetInventoryCount();
+    Print("INVENTORY COUNT: {}", count);
+
+    auto items = player.GetInventoryItems();
+    for (auto& item : items) {
+        Print("ITEM PTR: {:x}", (uintptr_t)item.item);
+        Print("ITEM QUANTITY: {}", item.quantity);
+        Print("ITEM PID: {}", item.item->prototypeID);
+    }
+
+    // Find the address!
+    auto targetAddress = FindPattern(
+        L"falloutwHR.exe", 0x10000, "\x8B\x72\x04\xEB\x11\xBB\x01\x00\x00\x00\xBA\x00\x00\x00\x00\x89\xC8",
+        "xxxxxxxxxxx????xx"
+    );
+    Print("Found address: {:x}", targetAddress);
+}
+
 void Injected_DLL_Main() {
     Fallout1::RE::MODULE_BASE = DllInjector::MODULE_BASE;
-    DoHookingBadassery();
+    // DoHookingBadassery();
+    // PrintOutFalloutStuff();
+    FindEntityList();
 }
