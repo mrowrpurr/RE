05916b80498cbc26fb1a14bd3f3c4c1b1cd0590d
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-03 22:42:35 -0800

    Restore all registers manually

--- Libraries/Cpp/LibRE/include/RE/Hooks.h
+++ Libraries/Cpp/LibRE/include/RE/Hooks.h
@@ -8,6 +8,7 @@
 #include <string_format.h>
 #include <xbyak/xbyak.h>
 
+#include <atomic>
 #include <functional>
 #include <memory>
 #include <optional>
@@ -22,22 +23,34 @@
 namespace RE::Hooks {
 
     /** [BEGIN] Refactor me! */
-    DWORD             CURRENT_REGISTER_VALUE_EAX   = 0;
-    DWORD             CURRENT_REGISTER_VALUE_EBX   = 0;
-    DWORD             CURRENT_REGISTER_VALUE_ECX   = 0;
-    DWORD             CURRENT_REGISTER_VALUE_EDX   = 0;
-    DWORD             CURRENT_REGISTER_VALUE_ESI   = 0;
-    DWORD             CURRENT_REGISTER_VALUE_EDI   = 0;
-    DWORD             CURRENT_REGISTER_VALUE_EBP   = 0;
-    DWORD             CURRENT_REGISTER_VALUE_ESP   = 0;
-    std::vector<BYTE> MOV_REGISTER_INSTRUCTION_EAX = {0xA3};
-    std::vector<BYTE> MOV_REGISTER_INSTRUCTION_EBX = {0x89, 0x1D};
-    std::vector<BYTE> MOV_REGISTER_INSTRUCTION_ECX = {0x89, 0x0D};
-    std::vector<BYTE> MOV_REGISTER_INSTRUCTION_EDX = {0x89, 0x15};
-    std::vector<BYTE> MOV_REGISTER_INSTRUCTION_ESI = {0x89, 0x35};
-    std::vector<BYTE> MOV_REGISTER_INSTRUCTION_EDI = {0x89, 0x3D};
-    std::vector<BYTE> MOV_REGISTER_INSTRUCTION_EDP = {0x89, 0x2D};
-    std::vector<BYTE> MOV_REGISTER_INSTRUCTION_ESP = {0x89, 0x25};
+    std::atomic<unsigned int> NEXT_HOOK_ID               = 69;
+    DWORD                     CURRENT_REGISTER_VALUE_EAX = 0;
+    DWORD                     CURRENT_REGISTER_VALUE_EBX = 0;
+    DWORD                     CURRENT_REGISTER_VALUE_ECX = 0;
+    DWORD                     CURRENT_REGISTER_VALUE_EDX = 0;
+    DWORD                     CURRENT_REGISTER_VALUE_ESI = 0;
+    DWORD                     CURRENT_REGISTER_VALUE_EDI = 0;
+    DWORD                     CURRENT_REGISTER_VALUE_EBP = 0;
+    DWORD                     CURRENT_REGISTER_VALUE_ESP = 0;
+
+    std::vector<BYTE> MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EAX = {0xA3};
+    std::vector<BYTE> MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EBX = {0x89, 0x1D};
+    std::vector<BYTE> MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ECX = {0x89, 0x0D};
+    std::vector<BYTE> MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDX = {0x89, 0x15};
+    std::vector<BYTE> MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESI = {0x89, 0x35};
+    std::vector<BYTE> MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDI = {0x89, 0x3D};
+    std::vector<BYTE> MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDP = {0x89, 0x2D};
+    std::vector<BYTE> MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESP = {0x89, 0x25};
+
+    std::vector<BYTE> MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EAX = {0x8B, 0x05};
+    std::vector<BYTE> MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EBX = {0x8B, 0x1D};
+    std::vector<BYTE> MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ECX = {0x8B, 0x0D};
+    std::vector<BYTE> MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDX = {0x8B, 0x15};
+    std::vector<BYTE> MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESI = {0x8B, 0x35};
+    std::vector<BYTE> MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDI = {0x8B, 0x3D};
+    std::vector<BYTE> MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDP = {0x8B, 0x2D};
+    std::vector<BYTE> MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESP = {0x8B, 0x25};
+
     class Registers {
         Registers()                            = default;
         ~Registers()                           = default;
@@ -60,8 +73,13 @@ namespace RE::Hooks {
         DWORD ebp() { return CURRENT_REGISTER_VALUE_EBP; }
         DWORD esp() { return CURRENT_REGISTER_VALUE_ESP; }
     };
+    // std::unordered_map<unsigned int, std::function<void(Registers&)>> THE_HOOK_LAMBDAS;
     std::function<void(Registers&)> THE_HOOK_LAMBDA;
-    void                            RUN_THE_LAMBDA() { THE_HOOK_LAMBDA(Registers::GetCurrent()); }
+
+    void RUN_A_LAMBDA(uint32_t hookId) {
+        // THE_HOOK_LAMBDA(Registers::GetCurrent());
+        FormApp::App().AppendOutput(string_format("RUN_A_LAMBDA() called by hook: {}", hookId));
+    }
     /** [END] Refactor me! */
 
     constexpr auto DEFAULT_HOOK_LENGTH = 5;
@@ -91,6 +109,8 @@ namespace RE::Hooks {
     }
 
     class Hook {
+        uint32_t _hookId = NEXT_HOOK_ID++;
+
         // An arbitrary name for the hook (must be unique)
         std::string _name;
 
@@ -136,15 +156,9 @@ namespace RE::Hooks {
             if (!_enabled) {
                 ReadOriginalBytes();
 
-                auto registerMovInstructionsTotalSize =
-                    MOV_REGISTER_INSTRUCTION_EAX.size() + MOV_REGISTER_INSTRUCTION_EBX.size() +
-                    MOV_REGISTER_INSTRUCTION_ECX.size() + MOV_REGISTER_INSTRUCTION_EDX.size() +
-                    MOV_REGISTER_INSTRUCTION_ESI.size() + MOV_REGISTER_INSTRUCTION_EDI.size() +
-                    MOV_REGISTER_INSTRUCTION_EDP.size() + MOV_REGISTER_INSTRUCTION_ESP.size();
-
-                auto size = _bytes.size() + registerMovInstructionsTotalSize + 1 + 5 + 1 +
-                            5;  // <original bytes>, <mov register bytes>, 1 byte for pushad, 5 bytes for call, 1 byte
-                                // for popad,
+                auto size = _bytes.size() + (5 + (6 * 7)) + (6 * 8) + 1 + 5 + 1 + 5 +
+                            5;  // <original bytes>, <mov register bytes>, 1 byte for pushad, 5 bytes for push, 5 bytes
+                                // for call, 1 byte for popad, <mov registers back, because sadness!>
 
                 auto memory = (LPBYTE)VirtualAlloc(NULL, size, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
                 memcpy_s(memory, _bytes.size(), _bytes.data(), _bytes.size());
@@ -152,66 +166,66 @@ namespace RE::Hooks {
 
                 // Copy the registers
                 memcpy_s(
-                    memory + offset, MOV_REGISTER_INSTRUCTION_EAX.size(), MOV_REGISTER_INSTRUCTION_EAX.data(),
-                    MOV_REGISTER_INSTRUCTION_EAX.size()
+                    memory + offset, MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EAX.size(),
+                    MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EAX.data(), MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EAX.size()
                 );
-                offset += MOV_REGISTER_INSTRUCTION_EAX.size();
+                offset += MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EAX.size();
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EAX;
                 offset += sizeof(DWORD);  // maybe this style is better
 
                 memcpy_s(
-                    memory + offset, MOV_REGISTER_INSTRUCTION_EBX.size(), MOV_REGISTER_INSTRUCTION_EBX.data(),
-                    MOV_REGISTER_INSTRUCTION_EBX.size()
+                    memory + offset, MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EBX.size(),
+                    MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EBX.data(), MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EBX.size()
                 );
-                offset += MOV_REGISTER_INSTRUCTION_EBX.size();
+                offset += MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EBX.size();
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EBX;
                 offset += 4;
 
                 memcpy_s(
-                    memory + offset, MOV_REGISTER_INSTRUCTION_ECX.size(), MOV_REGISTER_INSTRUCTION_ECX.data(),
-                    MOV_REGISTER_INSTRUCTION_ECX.size()
+                    memory + offset, MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ECX.size(),
+                    MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ECX.data(), MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ECX.size()
                 );
-                offset += MOV_REGISTER_INSTRUCTION_ECX.size();
+                offset += MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ECX.size();
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_ECX;
                 offset += 4;
 
                 memcpy_s(
-                    memory + offset, MOV_REGISTER_INSTRUCTION_EDX.size(), MOV_REGISTER_INSTRUCTION_EDX.data(),
-                    MOV_REGISTER_INSTRUCTION_EDX.size()
+                    memory + offset, MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDX.size(),
+                    MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDX.data(), MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDX.size()
                 );
-                offset += MOV_REGISTER_INSTRUCTION_EDX.size();
+                offset += MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDX.size();
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EDX;
                 offset += 4;
 
                 memcpy_s(
-                    memory + offset, MOV_REGISTER_INSTRUCTION_ESI.size(), MOV_REGISTER_INSTRUCTION_ESI.data(),
-                    MOV_REGISTER_INSTRUCTION_ESI.size()
+                    memory + offset, MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESI.size(),
+                    MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESI.data(), MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESI.size()
                 );
-                offset += MOV_REGISTER_INSTRUCTION_ESI.size();
+                offset += MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESI.size();
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_ESI;
                 offset += 4;
 
                 memcpy_s(
-                    memory + offset, MOV_REGISTER_INSTRUCTION_EDI.size(), MOV_REGISTER_INSTRUCTION_EDI.data(),
-                    MOV_REGISTER_INSTRUCTION_EDI.size()
+                    memory + offset, MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDI.size(),
+                    MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDI.data(), MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDI.size()
                 );
-                offset += MOV_REGISTER_INSTRUCTION_EDI.size();
+                offset += MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDI.size();
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EDI;
                 offset += 4;
 
                 memcpy_s(
-                    memory + offset, MOV_REGISTER_INSTRUCTION_EDP.size(), MOV_REGISTER_INSTRUCTION_EDP.data(),
-                    MOV_REGISTER_INSTRUCTION_EDP.size()
+                    memory + offset, MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDP.size(),
+                    MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDP.data(), MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDP.size()
                 );
-                offset += MOV_REGISTER_INSTRUCTION_EDP.size();
+                offset += MOV_REGISTER_TO_ADDRESS_INSTRUCTION_EDP.size();
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EBP;
                 offset += 4;
 
                 memcpy_s(
-                    memory + offset, MOV_REGISTER_INSTRUCTION_ESP.size(), MOV_REGISTER_INSTRUCTION_ESP.data(),
-                    MOV_REGISTER_INSTRUCTION_ESP.size()
+                    memory + offset, MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESP.size(),
+                    MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESP.data(), MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESP.size()
                 );
-                offset += MOV_REGISTER_INSTRUCTION_ESP.size();
+                offset += MOV_REGISTER_TO_ADDRESS_INSTRUCTION_ESP.size();
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_ESP;
                 offset += 4;
 
@@ -219,11 +233,19 @@ namespace RE::Hooks {
                 memory[offset] = 0x60;  // pushad
                 offset++;
 
+                // Change ...
+                memory[offset] = 0x68;  // push
+                offset++;
+
+                DWORD hookId = _hookId;
+                memcpy_s(&memory[offset], sizeof(DWORD), &hookId, sizeof(DWORD));
+                offset += 4;
+
                 memory[offset] = 0xE8;  // call
                 offset++;
 
                 THE_HOOK_LAMBDA             = _detourFunction.value();
-                DWORD_PTR myFunctionAddress = reinterpret_cast<DWORD_PTR>(RUN_THE_LAMBDA);
+                DWORD_PTR myFunctionAddress = reinterpret_cast<DWORD_PTR>(RUN_A_LAMBDA);
 
                 auto relativeDetourFunctionAddress        = myFunctionAddress - ((DWORD)memory + offset - 1) - 5;
                 *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)relativeDetourFunctionAddress;
@@ -231,6 +253,71 @@ namespace RE::Hooks {
 
                 memory[offset] = 0x61;  // popad
                 offset++;
+
+                // Back back into the registers
+                memcpy_s(
+                    memory + offset, MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EAX.size(),
+                    MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EAX.data(), MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EAX.size()
+                );
+                offset += MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EAX.size();
+                *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EAX;
+                offset += 4;
+
+                memcpy_s(
+                    memory + offset, MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EBX.size(),
+                    MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EBX.data(), MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EBX.size()
+                );
+                offset += MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EBX.size();
+                *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EBX;
+                offset += 4;
+
+                memcpy_s(
+                    memory + offset, MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ECX.size(),
+                    MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ECX.data(), MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ECX.size()
+                );
+                offset += MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ECX.size();
+                *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_ECX;
+                offset += 4;
+
+                memcpy_s(
+                    memory + offset, MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDX.size(),
+                    MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDX.data(), MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDX.size()
+                );
+                offset += MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDX.size();
+                *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EDX;
+                offset += 4;
+
+                memcpy_s(
+                    memory + offset, MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESI.size(),
+                    MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESI.data(), MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESI.size()
+                );
+                offset += MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESI.size();
+                *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_ESI;
+                offset += 4;
+
+                memcpy_s(
+                    memory + offset, MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDI.size(),
+                    MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDI.data(), MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDI.size()
+                );
+                offset += MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDI.size();
+                *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EDI;
+                offset += 4;
+
+                memcpy_s(
+                    memory + offset, MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDP.size(),
+                    MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDP.data(), MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDP.size()
+                );
+                offset += MOV_ADDRESS_TO_REGISTER_INSTRUCTION_EDP.size();
+                *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_EBP;
+                offset += 4;
+
+                memcpy_s(
+                    memory + offset, MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESP.size(),
+                    MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESP.data(), MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESP.size()
+                );
+                offset += MOV_ADDRESS_TO_REGISTER_INSTRUCTION_ESP.size();
+                *(uintptr_t*)((uintptr_t)&memory[offset]) = (uintptr_t)&CURRENT_REGISTER_VALUE_ESP;
+                offset += 4;
 
                 memory[offset] = 0xE9;  // jmp
                 offset++;
