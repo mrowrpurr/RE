651d6a85b5092b703fc1624474fab95c677a679e
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-14 00:10:39 -0700

    Read the names of Fallout items from .dat file

--- Games/Fallout1/InjectableDLLs/Fallout1InjectableDLL/Fallout1InjectableDLL.cpp
+++ Games/Fallout1/InjectableDLLs/Fallout1InjectableDLL/Fallout1InjectableDLL.cpp
@@ -1,10 +1,21 @@
 #include <Assembly.h>
-#include <CodeInjection.h>
+// #include <CodeInjection.h>
+#include <DatFile.h>
+#include <DatFileItem.h>
 #include <Injected_DLL.h>
 #include <Logging.h>
 #include <Memory.h>
 #include <StringFormatting.h>
 #include <UserInterface.h>
+
+#include <cstdint>
+#include <string>
+
+using namespace DatUnpacker;
+
+void ToLower(std::string& str) {
+    for (auto& c : str) c = tolower(c);
+}
 
 #define Output(...) UserInterface::App().AppendOutput(string_format(__VA_ARGS__))
 
@@ -14,39 +25,57 @@ std::string PrintBytes(std::vector<uint8_t> bytes) {
     return result;
 }
 
-void SetupHooks() {
-    auto dropItemHook =
-        CodeInjection::New("Drop Item")
-            .Configure([](Injection& _) {
-                _.Var<uintptr_t>("Detour", 0x46a41c);
-                _.Var<size_t>("DetourSize", 5);
-                _.Var<uintptr_t>(
-                    "JumpBack", _.Var<uintptr_t>("Detour") + _.Var<size_t>("DetourSize")
-                );
-                _.Var<uintptr_t>("Trampoline", 0);
-                _.ReadBytes(
-                    {.addressVariable = "Detour",
-                     .outVariable     = "OriginalBytes",
-                     .byteCount       = _.Var<size_t>("DetourSize")}
-                );
-            })
-            .OnInstall([](Injection& _) {
-                // _.AllocateMemory({.addressVariable = "Trampoline", .block = [](Injection&) {}});
-                _.WriteBytes({
-                    .addressVariable = "Detour",
-                    .bytes           = {0x69, 0x42, 0x69, 0x42, 0x69},
-                });
-            })
-            .OnUninstall([](Injection& _) {
-                _.WriteBytes({
-                    .addressVariable = "Detour",
-                    .bytesVariable   = "OriginalBytes",
-                });
-            });
+DatFile* datFile = nullptr;
+
+std::string GetNameOfItem(unsigned int prototypeId) {
+    auto items = datFile->items();
+    if (items == nullptr) return "Items was nullptr";
+    for (auto* item : *items) {
+        auto name = item->name();
+        ToLower(name);
+        if (name.ends_with("pro_item.msg") && name.contains("english")) {
+            auto* text             = item->data();
+            auto  lineNumber       = 0;
+            auto  targetLineNumber = prototypeId * 2 - 2;
+            for (auto* line = text; *line != '\0'; ++line) {
+                if (*line == '\n') {
+                    ++lineNumber;
+                    if (lineNumber == targetLineNumber) {
+                        auto* nameStart = line + 1;
+                        auto* nameEnd   = nameStart;
+                        while (*nameEnd != '\n') ++nameEnd;
+                        return std::string{nameStart, nameEnd};
+                    }
+                }
+            }
+        }
+    }
+    return "Unknown";
+}
+
+void ReadPlayerInventory() {
+    auto moduleBase         = 0x400000;  // Hardcoded for now
+    auto staticCharacterPtr = moduleBase + 0x262844;
+
+    auto playerInventoryPtr = Memory::Read<uint32_t>(staticCharacterPtr, {0x0, 0x34});
+
+    uintptr_t itemPtr  = 0;
+    size_t    quantity = 0;
+
+    while (itemPtr != 0xCCCCCCCC) {
+        itemPtr  = Memory::Read<uintptr_t>(playerInventoryPtr, 0x0);
+        quantity = Memory::Read<uint32_t>(playerInventoryPtr, 0x4);
+        playerInventoryPtr += 0x8;
+        if (itemPtr == 0xCCCCCCCC) break;
+
+        auto itemPID  = Memory::Read<uint32_t>(itemPtr, 0x64);
+        auto itemName = GetNameOfItem(itemPID);
+        Output("The player has {} items of item {} (ID {})\n", quantity, itemName, itemPID);
+    }
 }
 
 void RunUI() {
-    auto injections = CodeInjection::RegisteredInjections;
+    // auto injections = CodeInjection::RegisteredInjections;
 
     UserInterface::Run([&](auto& app) {
         app.SetTitle("Fallout 1")
@@ -54,22 +83,25 @@ void RunUI() {
             .SetHeight(500)
             .SetWidth(500)
             .ShowOutputTextBox();
-        for (auto [name, injection] : injections) {
-            app.AddButton(string_format("Enable {}", injection->GetName()), [&, injection]() {
-                injection->Toggle();
-                if (injection->IsInstalled())
-                    app.ChangeButtonText(string_format("Disable {}", injection->GetName()));
-                else
-                    app.ChangeButtonText(string_format("Enable {}", injection->GetName()));
-            });
-        }
+        app.AddButton("Read Player Inventory", [&]() { ReadPlayerInventory(); });
+        // for (auto [name, injection] : injections) {
+        //     app.AddButton(string_format("Enable {}", injection->GetName()), [&, injection]() {
+        //         injection->Toggle();
+        //         if (injection->IsInstalled())
+        //             app.ChangeButtonText(string_format("Disable {}", injection->GetName()));
+        //         else
+        //             app.ChangeButtonText(string_format("Enable {}", injection->GetName()));
+        //     });
+        // }
         app.AddButton("Clear", [&]() { app.ClearOutput(); });
         app.AddButton("Eject DLL", [&]() { app.Close(); });
     });
 }
 
 DLL_Main {
-    SetupHooks();
+    datFile = new DatFile{"D:\\GOG\\Games\\Fallout\\MASTER.DAT"};
+    datFile->version();  // LOL
+    // SetupHooks();
     RunUI();
     Injected_DLL::EjectDLL();
 }
@@ -79,3 +111,47 @@ DLL_Main {
 //         code.nop();
 //         code.nop();
 //     })
+
+// void SetupHooks() {
+//     auto dropItemHook =
+//         CodeInjection::New("Drop Item")
+//             .Configure([](Injection& _) {
+//                 _.Var<uintptr_t>("Detour", 0x46a41c);
+//                 _.Var<size_t>("DetourSize", 5);
+//                 _.Var<uintptr_t>(
+//                     "JumpBack", _.Var<uintptr_t>("Detour") + _.Var<size_t>("DetourSize")
+//                 );
+//                 _.Var<uintptr_t>("Trampoline", 0);
+//                 _.ReadBytes(
+//                     {.addressVariable = "Detour",
+//                      .outVariable     = "OriginalBytes",
+//                      .byteCount       = _.Var<size_t>("DetourSize")}
+//                 );
+//             })
+//             .OnInstall([](Injection& _) {
+//                 // _.AllocateMemory({.addressVariable = "Trampoline", .block = [](Injection&)
+//                 {}});
+//                 _.WriteBytes({
+//                     .addressVariable = "Detour",
+//                     .bytes           = {0x69, 0x42, 0x69, 0x42, 0x69},
+//                 });
+//             })
+//             .OnUninstall([](Injection& _) {
+//                 _.WriteBytes({
+//                     .addressVariable = "Detour",
+//                     .bytesVariable   = "OriginalBytes",
+//                 });
+//             });
+// }
+
+// void BeginTrackingEntities() {
+//     //
+// }
+
+// void PrintKnownEntities() {
+//     //
+// }
+
+// void ClearKnownEntities() {
+//     //
+// }
--- Libraries/Cpp/Memory/include/Memory/Reader.h
+++ Libraries/Cpp/Memory/include/Memory/Reader.h
@@ -5,9 +5,36 @@
 
 namespace Memory {
 
-    std::vector<uint8_t> ReadBytes(uintptr_t address, size_t size) {
+    std::vector<uint8_t> Read(uintptr_t address, size_t size) {
         std::vector<uint8_t> bytes(size);
         for (size_t i = 0; i < size; i++) bytes[i] = *reinterpret_cast<uint8_t*>(address + i);
         return bytes;
+    }
+
+    template <typename T>
+    T Read(uintptr_t address) {
+        return (T)(address);
+    }
+
+    template <typename T>
+    T Read(uintptr_t address, uint32_t offset) {
+        address = *reinterpret_cast<uintptr_t*>(address + offset);
+        return (T)(address);
+    }
+
+    template <typename T>
+    T Read(uintptr_t address, std::vector<uint32_t> offsets) {
+        for (auto offset : offsets) address = *reinterpret_cast<uintptr_t*>(address + offset);
+        return (T)(address);
+    }
+
+    template <typename T>
+    T Read(uintptr_t address, uint32_t offset1, uint32_t offset2) {
+        return Read<T>(address, {offset1, offset2});
+    }
+
+    template <typename T>
+    T Read(uintptr_t address, uint32_t offset1, uint32_t offset2, uint32_t offset3) {
+        return Read<T>(address, {offset1, offset2, offset3});
     }
 }
