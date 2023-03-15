afb5aaa8bcf256138d20f0b5e1e6e91d5ddb3928
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-15 08:17:25 -0700

    Memory::Bytes

--- Libraries/Cpp/Memory/include/Memory.h
+++ Libraries/Cpp/Memory/include/Memory.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Memory/Bytes.h"
 #include "Memory/Reader.h"
 #include "Memory/Search.h"
 #include "Memory/Writer.h"
---
+++ Libraries/Cpp/Memory/include/Memory/Bytes.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <StringFormatting.h>
+
+#include <cstdint>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace Memory {
+
+    class Bytes {
+        std::vector<uint8_t> _bytes;
+
+    public:
+        Bytes() = default;
+        Bytes(std::vector<uint8_t> bytes) : _bytes(bytes) {}
+
+        std::vector<uint8_t> GetBytes() { return _bytes; }
+        size_t               GetByteCount() { return _bytes.size(); }
+
+        void AddByte(uint8_t byte) { _bytes.push_back(byte); }
+        void AddBytes(std::vector<uint8_t> bytes) {
+            for (auto byte : bytes) {
+                _bytes.push_back(byte);
+            }
+        }
+
+        void Add8(uint8_t byte) { AddByte(byte); }
+        void Add16(uint16_t word) {
+            _bytes.push_back((word >> 0) & 0xFF);
+            _bytes.push_back((word >> 8) & 0xFF);
+        }
+        void Add32(uint32_t dword) {
+            _bytes.push_back((dword >> 0) & 0xFF);
+            _bytes.push_back((dword >> 8) & 0xFF);
+            _bytes.push_back((dword >> 16) & 0xFF);
+            _bytes.push_back((dword >> 24) & 0xFF);
+        }
+        void Add64(uint64_t qword) {
+            _bytes.push_back((qword >> 0) & 0xFF);
+            _bytes.push_back((qword >> 8) & 0xFF);
+            _bytes.push_back((qword >> 16) & 0xFF);
+            _bytes.push_back((qword >> 24) & 0xFF);
+            _bytes.push_back((qword >> 32) & 0xFF);
+            _bytes.push_back((qword >> 40) & 0xFF);
+            _bytes.push_back((qword >> 48) & 0xFF);
+            _bytes.push_back((qword >> 56) & 0xFF);
+        }
+
+        void AddWord(uint16_t word) { Add16(word); }
+        void AddDword(uint32_t dword) { Add32(dword); }
+        void AddQword(uint64_t qword) { Add64(qword); }
+
+        void AddAddress(uintptr_t address) {
+            if (sizeof(uintptr_t) == 4) Add32(address);
+            else if (sizeof(uintptr_t) == 8) Add64(address);
+            else
+                throw std::runtime_error(
+                    string_format("Bytes: Unsupported address size {}", sizeof(uintptr_t))
+                );
+        }
+
+        std::string ToString() {
+            std::string str;
+            for (auto byte : _bytes) str += string_format("{:x} ", byte);
+            return str;
+        }
+    };
+
+}
