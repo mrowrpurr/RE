1206e5491f438ce2b2045e2e3a9492ca1487e683
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-15 08:17:33 -0700

    Add Logging

--- Libraries/Cpp/Memory/include/Memory/Writer.h
+++ Libraries/Cpp/Memory/include/Memory/Writer.h
@@ -1,18 +1,24 @@
 #pragma once
 
+#include <Logging.h>
+#include <StringFormatting.h>
 #include <Windows.h>
 
 #include <cstdint>
 
+#include "Bytes.h"
+
 namespace Memory {
 
     void WriteBytes(uintptr_t address, const std::vector<uint8_t>& bytes) {
-        for (size_t i = 0; i < bytes.size(); i++) {
-            *(uint8_t*)(address + i) = bytes[i];
-        }
+        Log("Writing {} bytes to address {:x}", bytes.size(), address);
+        if (bytes.size() < 128) Log(Bytes{bytes}.ToString());
+        for (size_t i = 0; i < bytes.size(); i++) *(uint8_t*)(address + i) = bytes[i];
     }
 
     void WriteProtectedBytes(uintptr_t address, const std::vector<uint8_t>& bytes) {
+        Log("Writing {} protected bytes to address {:x}", bytes.size(), address);
+        if (bytes.size() < 128) Log(Bytes{bytes}.ToString());
         DWORD oldProtect;
         VirtualProtect((void*)address, bytes.size(), PAGE_EXECUTE_READWRITE, &oldProtect);
         memcpy((void*)address, bytes.data(), bytes.size());
@@ -20,9 +26,17 @@ namespace Memory {
     }
 
     uintptr_t Allocate(size_t size) {
-        auto address = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-        return reinterpret_cast<uintptr_t>(address);
+        Log("Allocating {} bytes", size);
+        auto addressPtr = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+        if (addressPtr == nullptr) throw std::runtime_error("Failed to allocate memory");
+        auto address = reinterpret_cast<uintptr_t>(addressPtr);
+        Log("Allocated at address {:x}", address);
+        return address;
     }
 
-    void Free(uintptr_t address) { VirtualFree((void*)address, 0, MEM_RELEASE); }
+    void Free(uintptr_t address) {
+        Log("Freeing memory at address {:x}", address);
+        if (VirtualFree((void*)address, 0, MEM_RELEASE)) Log("Freed memory at address {:x}", address);
+        else throw std::runtime_error(string_format("Failed to free memory at address {:x}", address));
+    }
 }
