564e385a6d5f027a9dbb40cd43a9632c1429b82e
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-18 14:59:24 -0700

    Call C++ function from Lua

--- Examples/LetsEmbedSomeLuaShallWe/LetsEmbedSomeLuaShallWe.cpp
+++ Examples/LetsEmbedSomeLuaShallWe/LetsEmbedSomeLuaShallWe.cpp
@@ -14,6 +14,30 @@ std::string PathToScript(const std::string& scriptName) {
 
 #include <iostream>
 
+void report_lua_errors(lua_State* luaState, int status) {
+    if (status == 0) return;
+    std::cerr << "[LUA ERROR] " << lua_tostring(luaState, -1) << std::endl;
+    lua_pop(luaState, 1);  // remove error message from Lua state
+}
+
+int Cpp_Function_To_Call_From_Lua() { return 69; }
+
+void Call_Cpp_Function_From_Lua() {
+    auto       scriptPath = PathToScript("Call_Cpp_Function.lua");
+    lua_State* L          = luaL_newstate();
+    luaL_openlibs(L);
+
+    // Make C++ function Cpp_Function_To_Call_From_Lua available to Lua
+    luabridge::getGlobalNamespace(L)
+        .beginNamespace("Cpp")
+        .addFunction("Cpp_Function_To_Call_From_Lua", Cpp_Function_To_Call_From_Lua)
+        .endNamespace();
+
+    auto status = luaL_dofile(L, scriptPath.c_str());
+    report_lua_errors(L, status);
+    lua_close(L);
+}
+
 void Call_Lua_String_Lua_From_Cpp() {
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
@@ -32,5 +56,6 @@ int main() {
     std::cout << "[BEGIN]" << std::endl;
     Call_Lua_String_Lua_From_Cpp();
     Call_Lua_File_Script_From_Cpp();
+    Call_Cpp_Function_From_Lua();
     std::cout << "[END]" << std::endl;
 }
---
+++ Examples/LetsEmbedSomeLuaShallWe/scripts/Call_Cpp_Function.lua
@@ -0,0 +1,4 @@
+-- Call a C++ function called Cpp_Function_To_Call_From_Lua
+
+result = Cpp.Cpp_Function_To_Call_From_Lua()
+print("Result from C++ function: " .. result)
