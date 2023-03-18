33803809dcd1694b0aacf9609535304b53c78328
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-17 21:24:23 -0700

    Freeze Weight Injection

--- Games/Fallout1/InjectableDLLs/Fallout1InjectableDLL/Fallout1InjectableDLL.cpp
+++ Games/Fallout1/InjectableDLLs/Fallout1InjectableDLL/Fallout1InjectableDLL.cpp
@@ -11,30 +11,75 @@
 
 SetLogFilePath("InjectedDLL.log");
 
-#define Output(...) UserInterface::App().AppendOutput(string_format(__VA_ARGS__))
+#define Output(...)                    \
+    UserInterface::App().AppendOutput( \
+        string_format(__VA_ARGS__)     \
+    )
 
 void SetupHooks() {
-    CodeInjection::InjectFunction<11>("Drop Item", 0x46a41c, [](Registers& regs) {
+    CodeInjection::InjectFunction<
+        11>("Drop Item", 0x46a41c, [](Registers& regs) {
         Output("I am a function and I was called!");
         Output("EAX is {:x}", regs.eax());
         Output("EAX 0x0 is {}", regs.eax(0x0));
-        Output("EAX 0x34 0x0 is {:x}", regs.eax({0x34, 0x0}));
+        Output(
+            "EAX 0x34 0x0 is {:x}", regs.eax({0x34, 0x0})
+        );
         Output("EAX 0x34 0x4 is {}", regs.eax({0x34, 0x4}));
-        Output("EAX 0x34 0x0 0x64 is {}", regs.eax({0x34, 0x0, 0x64}));
+        Output(
+            "EAX 0x34 0x0 0x64 is {}",
+            regs.eax({0x34, 0x0, 0x64})
+        );
     });
+
+    CodeInjection::New("Freeze Carry Weight")
+        .Configure([](Injection& _) {
+            _.Var<uintptr_t>("OverwriteAddress", 0x49ceca);
+            _.ReadBytes(
+                "OverwriteAddress", "OriginalBytes", 3
+            );
+        })
+        .OnInstall([](Injection& _) {
+            _.WriteNop("OverwriteAddress", 3);
+        })
+        .OnUninstall([](Injection& _) {
+            _.WriteBytes(
+                "OverwriteAddress", "OriginalBytes"
+            );
+        });
 }
 
 void RunUI() {
     UserInterface::Run([&](auto& app) {
-        app.SetTitle("Fallout 1").SetButtonHeight(50).SetHeight(500).SetWidth(500).ShowOutputTextBox();
-        for (auto [name, injection] : CodeInjection::RegisteredInjections) {
-            app.AddButton(string_format("Enable {}", injection->GetName()), [&, injection]() {
-                injection->Toggle();
-                if (injection->IsInstalled()) app.ChangeButtonText(string_format("Disable {}", injection->GetName()));
-                else app.ChangeButtonText(string_format("Enable {}", injection->GetName()));
-            });
+        app.SetTitle("Fallout 1")
+            .SetButtonHeight(50)
+            .SetHeight(500)
+            .SetWidth(500)
+            .ShowOutputTextBox();
+        for (auto [name, injection] :
+             CodeInjection::RegisteredInjections) {
+            app.AddButton(
+                string_format(
+                    "Enable {}", injection->GetName()
+                ),
+                [&, injection]() {
+                    injection->Toggle();
+                    if (injection->IsInstalled())
+                        app.ChangeButtonText(string_format(
+                            "Disable {}",
+                            injection->GetName()
+                        ));
+                    else
+                        app.ChangeButtonText(string_format(
+                            "Enable {}",
+                            injection->GetName()
+                        ));
+                }
+            );
         }
-        app.AddButton("Clear", [&]() { app.ClearOutput(); });
+        app.AddButton("Clear", [&]() {
+            app.ClearOutput();
+        });
         app.AddButton("Eject DLL", [&]() { app.Close(); });
     });
 }
