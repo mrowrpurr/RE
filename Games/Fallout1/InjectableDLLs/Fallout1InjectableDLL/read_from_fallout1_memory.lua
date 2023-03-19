9ed8862a01f89891fad66c6d948f3e478af7e5f6
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-19 14:57:11 -0700

    Read C struct from Fallout 1 in Lua

--- Games/Fallout1/InjectableDLLs/Fallout1InjectableDLL/read_from_fallout1_memory.lua
+++ Games/Fallout1/InjectableDLLs/Fallout1InjectableDLL/read_from_fallout1_memory.lua
@@ -2,10 +2,16 @@ local fallout1 = require("fallout1")
 
 local ffi = require("ffi")
 
+ffi.cdef[[
+    typedef struct { uint32_t objID, tileNum; } NPC;
+]]
+
 function CallMeMaybe()
     local output = ""
 
     output = output .. "Hello friends!\n"
+
+    output = output .. fallout1.butts() .. "\n"
 
     output = output .. fallout1.fallout1_function_from_init() .. "\n"
 
@@ -20,6 +26,10 @@ function CallMeMaybe()
 
     local player_tile_number = ffi.cast("uint32_t*", player_ptr + 0x4)[0]
     output = output .. "Player Tile Number: " .. string.format("%i", player_tile_number) .. "\n"
+
+    local player_struct = ffi.cast("NPC*", player_ptr)
+    output = output .. "Player Struct objID (should be same as player ID): " .. string.format("%i", player_struct.objID) .. "\n"
+    output = output .. "Player Struct tileNum (should be same as player tile number): " .. string.format("%i", player_struct.tileNum) .. "\n"
 
     return output
 end
--- Games/Fallout1/Libraries/Lua/Fallout1Lib/fallout1/init.lua
+++ Games/Fallout1/Libraries/Lua/Fallout1Lib/fallout1/init.lua
@@ -4,4 +4,8 @@ function x.fallout1_function_from_init()
     return "Hello from our fallout1 init.lua file!"
 end
 
+function x.butts()
+    return "butts"
+end
+
 return x 
