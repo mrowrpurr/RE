ed54f09316d0e34e7d67b35289e335a51a39794c
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-19 23:33:05 -0700

    Fallout 1 MO2 plugin

---
+++ Games/Fallout1/MO2/game_fallout1.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+from ..basic_game import BasicGame
+
+class Fallout1Game(BasicGame):
+    Name = "Fallout 1 - Support Plugin"
+    Author = "mrowrpurr"
+    Version = "0.1.0"
+
+    GameName = "Fallout 1"
+    GameShortName = "fallout"
+    GameNexusName = "fallout"
+    GameNexusId = 2667
+    GameSteamId = 38400
+    GameGogId = 1440148836
+    GameBinary = "falloutwHR.exe"
+    GameDataPath = "%GAME_PATH%"
+    GameDocumentsDirectory = "%GAME_PATH%"
