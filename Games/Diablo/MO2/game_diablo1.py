26fd7c899c83e12ed458764fa9b976f93ddd97fd
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-21 14:26:29 -0700

    Diablo 1 MO2 plugin

---
+++ Games/Diablo/MO2/game_diablo1.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+from ..basic_game import BasicGame
+
+class Diablo1Game(BasicGame):
+    Name = "Diablo 1 - Support Plugin"
+    Author = "mrowrpurr"
+    Version = "0.1.0"
+
+    GameName = "Diablo 1"
+    GameShortName = "diablo"
+    GameNexusName = "diablo"
+    GameNexusId = 1814
+    # GameSteamId = NONE
+    GameGogId = 1412601690
+    GameBinary = "Diablo.exe"
+    GameDataPath = "%GAME_PATH%"
+    GameDocumentsDirectory = "%GAME_PATH%"
