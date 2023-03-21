6f13916be065157b8cb7e4f0945ad9817368b8a3
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-21 16:21:56 -0700

    modding_framework.exe

--- ModdingFramework/Cpp/Executable/ModdingFramework.cpp
+++ ModdingFramework/Cpp/Executable/ModdingFramework.cpp
@@ -1,3 +1,28 @@
+#include <DLL_Injector.h>
+#include <Logging.h>
+#include <Serialization.h>
 #include <Windows.h>
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) { return 0; }
+#include <iostream>
+
+SetLogFilePath("modding_framework.log");
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    // TODO: make this not suck (don't directly use std::any)
+    auto config = any_cast<std::unordered_map<std::string, std::any>>(
+        Serialization::LoadFile("modding_framework.ini")["Modding Framework"]
+    );
+    auto game = any_cast<std::string>(config["game"]);
+
+    if (DLL_Injector::Util::IsProcessRunning(game))
+        DLL_Injector::InjectDLL(game, "modding_framework.dll");
+
+    // if (GameIsRunning)
+    //     InjectDll();
+    // else {
+    //      RunGameSuspended();
+    //     InjectDll();
+    // }
+
+    return 0;
+}
