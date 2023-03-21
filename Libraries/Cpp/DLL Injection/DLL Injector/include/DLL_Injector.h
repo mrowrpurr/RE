a70f94d52e853cd6a4df1d07899fc4296a968c05
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-20 21:34:26 -0700

    DLL Injector uses .ini file

--- Libraries/Cpp/DLL Injection/DLL Injector/include/DLL_Injector.h
+++ Libraries/Cpp/DLL Injection/DLL Injector/include/DLL_Injector.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Windows.h>
+
 // ^ require Windows.h first
 
 #include <TlHelp32.h>
@@ -10,17 +11,8 @@
 namespace DLL_Injector {
 
     namespace Util {
-        // https://stackoverflow.com/a/8032108
-        // CC BY-SA 3.0
-        const wchar_t* GetWideCharArray(const char* c) {
-            const size_t cSize = strlen(c) + 1;
-            wchar_t*     wc    = new wchar_t[cSize];
-            mbstowcs(wc, c, cSize);
-            return wc;
-        }
 
-        DWORD GetProcId(const wchar_t* exeName) {
-            // Get the process ID of the process that we want to inject our DLL into
+        DWORD GetProcId(const std::string& exeName) {
             DWORD  procId = 0;
             HANDLE hSnap  = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
             if (hSnap != INVALID_HANDLE_VALUE) {
@@ -28,7 +20,7 @@ namespace DLL_Injector {
                 procEntry.dwSize = sizeof(procEntry);
                 if (Process32First(hSnap, &procEntry)) {
                     do {
-                        if (!_wcsicmp(GetWideCharArray(procEntry.szExeFile), exeName)) {  // _stricmp
+                        if (!_stricmp(procEntry.szExeFile, exeName.c_str())) {
                             procId = procEntry.th32ProcessID;
                             break;
                         }
@@ -39,7 +31,7 @@ namespace DLL_Injector {
             return procId;
         }
 
-        DWORD WaitForProcId(const wchar_t* exeName, DWORD intervalMs = 30) {
+        DWORD WaitForProcId(const std::string& exeName, DWORD intervalMs = 30) {
             DWORD procId = 0;
             while (!procId) {
                 procId = GetProcId(exeName);
@@ -50,14 +42,20 @@ namespace DLL_Injector {
     }
 
     namespace Techniques {
-        void InjectDLL_CreateRemoteThread_LoadLibraryA(const wchar_t* exeName, const char* dllPath) {
+
+        void InjectDLL_CreateRemoteThread_LoadLibraryA(
+            const std::string& exeName, const std::string& dllPath
+        ) {
             auto procId = Util::WaitForProcId(exeName);
 
             HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
             if (hProc && hProc != INVALID_HANDLE_VALUE) {
-                void* loc = VirtualAllocEx(hProc, 0, MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-                WriteProcessMemory(hProc, loc, dllPath, strlen(dllPath) + 1, 0);
-                HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0);
+                void* loc =
+                    VirtualAllocEx(hProc, 0, MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+                WriteProcessMemory(hProc, loc, dllPath.c_str(), dllPath.size() + 1, 0);
+                HANDLE hThread = CreateRemoteThread(
+                    hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0
+                );
                 if (hThread) {
                     CloseHandle(hThread);
                 }
@@ -65,7 +63,7 @@ namespace DLL_Injector {
         }
     }
 
-    void InjectDLL(const wchar_t* exeName, const char* dllPath) {
+    void InjectDLL(const std::string& exeName, const std::string& dllPath) {
         Techniques::InjectDLL_CreateRemoteThread_LoadLibraryA(exeName, dllPath);
     }
 }
--- Tools/Cpp/DLL Injector UI/DLL_Injector_UI.cpp
+++ Tools/Cpp/DLL Injector UI/DLL_Injector_UI.cpp
@@ -1,35 +1,63 @@
+#include <Logging.h>
+SetLogFilePath("DLL_Injector_UI.log");
+//
+
 #include <DLL_Injector.h>
+#include <SimpleIni.h>
+#include <StringFormatting.h>
 #include <UserInterface.h>
 
-constexpr auto Fallout1_EXE = L"falloutwHR.exe";
-constexpr auto Fallout1_DLL =
-    "C:/Code/mrowrpurr/Retro-RE-Playground/build/debug/Games/Fallout1/InjectableDLLs/"
-    "Fallout1InjectableDLL/"
-    "Fallout1InjectableDLL.dll";
+#include <filesystem>
 
-constexpr auto Vampire_EXE = L"Vampire.exe";
-constexpr auto Vampire_DLL = "REBUILD ME!";
+constexpr auto ProjectRoot = "../../../../..";
+constexpr auto IniFileName = "dll_injector.ini";
 
-constexpr auto Oblivion_EXE = L"Oblivion.exe";
-constexpr auto Oblivion_DLL =
-    "C:/Code/mrowrpurr/Retro-RE-Playground/build/debug/Games/Oblivion/InjectableDLLs/Cpp/"
-    "OblivionInjectableDLL/OblivionInjectableDLL.dll";
+struct InjectableDLL {
+    std::string Name;
+    std::string DLL;
+    std::string EXE;
+};
 
-constexpr auto Diablo_EXE = L"Diablo.exe";
-constexpr auto Diablo_DLL =
-    "C:/Code/mrowrpurr/Retro-RE-Playground/build/debug/Games/Diablo/InjectableDLLs/Cpp/"
-    "DiabloInjectableDLL/DiabloInjectableDLL.dll";
+std::vector<InjectableDLL> GetDLLs() {
+    std::vector<InjectableDLL> DLLs;
+
+    CSimpleIniA ini;
+    auto iniLoadError = ini.LoadFile(string_format("{}/{}", ProjectRoot, IniFileName).c_str());
+
+    if (iniLoadError < 0) return DLLs;
+
+    CSimpleIniA::TNamesDepend DLLsNames;
+    ini.GetAllSections(DLLsNames);
+
+    for (auto& DLLName : DLLsNames) {
+        InjectableDLL DLL;
+        DLL.Name = DLLName.pItem;
+
+        auto dllRelativePathToProject = ini.GetValue(DLLName.pItem, "DLL", "");
+        auto dllAbsolutePath =
+            std::filesystem::absolute(string_format("{}/{}", ProjectRoot, dllRelativePathToProject)
+            );
+
+        DLL.DLL = std::filesystem::absolute(std::filesystem::path{dllAbsolutePath}).string();
+        DLL.EXE = ini.GetValue(DLLName.pItem, "EXE", "");
+        DLLs.push_back(DLL);
+    }
+
+    return DLLs;
+}
 
 UI_Main {
     UserInterface::Run([](auto& app) {
+        auto injectableDLLs = GetDLLs();
+
         app.SetTitle("Choose DLL to Inject");
-        app.SetWidth(600);
-        app.SetHeight(50);
-        app.AddButton("Fallout 1", [&]() { DLL_Injector::InjectDLL(Fallout1_EXE, Fallout1_DLL); });
-        app.AddButton("Vampire the Masquerade: Bloodlines", []() {
-            DLL_Injector::InjectDLL(Vampire_EXE, Vampire_DLL);
-        });
-        app.AddButton("Oblivion", []() { DLL_Injector::InjectDLL(Oblivion_EXE, Oblivion_DLL); });
-        app.AddButton("Diablo", []() { DLL_Injector::InjectDLL(Diablo_EXE, Diablo_DLL); });
+        app.SetHeight(15 * injectableDLLs.size());
+        app.SetWidth(500);
+
+        for (auto& injectableDLL : injectableDLLs) {
+            app.AddButton(injectableDLL.Name, [&, injectableDLL]() {
+                DLL_Injector::InjectDLL(injectableDLL.EXE, injectableDLL.DLL);
+            });
+        }
     });
 }
