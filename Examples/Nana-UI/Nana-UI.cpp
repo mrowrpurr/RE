840f2a0ad180b654706986ca736f9ff513780643
Mrowr Purr <mrowr.purr@gmail.com>
2023-03-28 18:24:00 -0700

    Basic tabs OK

--- Examples/Nana-UI/Nana-UI.cpp
+++ Examples/Nana-UI/Nana-UI.cpp
@@ -9,50 +9,77 @@
 int main() {
     using namespace nana;
 
-    // Create the main form
-    form fm{API::make_center(400, 300)};
-    fm.caption("Nana GUI App");
+    form fm{API::make_center(500, 300)};
+    fm.caption("Nana GUI Sample");
 
-    auto panel1 = panel<true>(fm);
-    auto panel2 = panel<true>(fm);
+    tabbar<std::string> tab(fm);
+    tab.push_back("Form");
+    tab.push_back("Controls");
 
-    // Create the tab bar and tabs
-    tabbar<std::string> tabs(fm, rectangle(10, 10, 380, 280));
-    tabs.append("Form with Labels and Textboxes", panel1);
-    tabs.append("Form with Sliders, Radio Buttons, and Checkboxes", panel2);
+    panel<true> form_panel(fm);
+    panel<true> controls_panel(fm);
 
-    label label1{panel1, "Name:"};
-    label1.move(30, 20);
-    textbox textbox1{panel1, rectangle(100, 20, 200, 20)};
+    // Form layout
+    place form_place{form_panel};
+    form_place.div("vert <labels gap=5><inputs gap=5>");
 
-    label label2{panel1, "Age:"};
-    label2.move(30, 60);
-    textbox textbox2{panel1, rectangle(100, 60, 200, 20)};
+    label   name_label{form_panel, "Name:"};
+    textbox name_box{form_panel};
 
-    // Second tab: Form with Sliders, Radio Buttons, and Checkboxes
-    slider slider1{panel2, rectangle(30, 20, 200, 20)};
-    slider slider2{panel2, rectangle(30, 60, 200, 20)};
+    label   age_label{form_panel, "Age:"};
+    textbox age_box{form_panel};
 
-    label label3{panel2, "Gender:"};
-    label3.move(240, 20);
-    radio_group gender_group;
-    checkbox    male_radio{panel2, "Male"};
-    gender_group.add(male_radio);
-    male_radio.move(240, 40);
+    form_place["labels"] << name_label << age_label;
+    form_place["inputs"] << name_box << age_box;
 
-    checkbox female_radio{panel2, "Female"};
-    gender_group.add(female_radio);
-    female_radio.move(240, 60);
+    form_place.collocate();
 
-    checkbox check1{panel2, "Check 1"};
-    check1.move(30, 100);
-    checkbox check2{panel2, "Check 2"};
-    check2.move(30, 130);
+    // Controls layout
+    place controls_place{controls_panel};
+    controls_place.div("vert <sliders gap=5><radios gap=5><checkboxes gap=5>");
 
-    // Show the form
+    label  slider_label{controls_panel, "Slider:"};
+    slider slider_ctrl{controls_panel};
+
+    label       radio_label{controls_panel, "Radio Group:"};
+    radio_group radio_grp;
+    checkbox    radio1{controls_panel, "Option 1"};
+    checkbox    radio2{controls_panel, "Option 2"};
+    radio_grp.add(radio1);
+    radio_grp.add(radio2);
+
+    label    checkbox_label{controls_panel, "Checkbox:"};
+    checkbox checkbox_ctrl{controls_panel, "Enable"};
+
+    controls_place["sliders"] << slider_label << slider_ctrl;
+    controls_place["radios"] << radio_label << radio1 << radio2;
+    controls_place["checkboxes"] << checkbox_label << checkbox_ctrl;
+
+    controls_place.collocate();
+
+    // Tab events
+    tab.events().activated([&](const arg_tabbar<std::string>& bar) {
+        if (bar.item_pos == 0) {
+            form_panel.show();
+            controls_panel.hide();
+        } else {
+            form_panel.hide();
+            controls_panel.show();
+        }
+        return 1;
+    });
+
+    // Layout for the main form
+    place main_place{fm};
+    main_place.div("<tab><panel>");
+    main_place["tab"] << tab;
+    main_place["panel"] << form_panel << controls_panel;
+    main_place.collocate();
+
+    // controls_panel.hide();
+    tab.activated(0);
     fm.show();
 
-    // Run the event loop
     exec();
     return 0;
 }
